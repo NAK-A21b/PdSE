7dc0d2f5783bcaf3a631b859eb70234bfa791be7
Nick Strodthoff <nick@krabbelmonster.de>
2022-10-24 13:45:08 +0200

    Add implementation of tournament and complete task

--- lesson2/task1/src/de/nordakademie/guessmynumber/Tournament.java
+++ lesson2/task1/src/de/nordakademie/guessmynumber/Tournament.java
@@ -4,28 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tournament {
-    /* TODO: Exemplarvariable für alle Spieler des Tournaments. Also ein Array.*/
+
+    private final IPlayer[] players;
     private final int rounds;
     private final IPayOffRule rule;
 
     /**
      * Dieser Konstruktor bekommt die rule die die Player injiziert.
+     *
      * @param rounds
      * @param rule
      * @param players
      */
-    public Tournament(int rounds, IPayOffRule rule, /* TODO: Der Parameter für Players. Statt eines Spielerarrays mit [] verwenden Sie bitte das Varargs Symbol ...  . Es erlaubt die Übergabe von mehreren Spielern */) {
+    public Tournament(int rounds, IPayOffRule rule, IPlayer... players) {
         this.players = players;
         this.rounds = rounds;
-        this.rule=rule;
+        this.rule = rule;
     }
 
 
-    public List<String> run(){
-        final List<String> results = new ArrayList<String>();
+    public List<String> run() {
+        final List<String> results = new ArrayList<>();
         for (int i = 0; i < players.length; i++) {
-            for (int j = /* TODO: Initialisieren Sie j so, dass jeder Sieler spielt gegen jeden anderen einmal matcht. */; j < players.length; j++) {
-                int result = /* TODO: Spieler i tritt in einem Match gegen Spieler j an. */;
+            for (int j = i + 1; j < players.length; j++) {
+                int result = matchPlayers(players[i], players[j]);
                 String pairing = players[i].getClass().getSimpleName() + " vs " + players[j].getClass().getSimpleName();
                 results.add(pairing + ": " + result);
             }
@@ -36,12 +38,16 @@ public class Tournament {
     /*
     Matches two players
      */
-    private int matchPlayers(IPlayer player1, IPlayer player2){
-        int result=0;
+    private int matchPlayers(IPlayer player1, IPlayer player2) {
+        GuessMyNumber forward = new GuessMyNumber(player1, player2, rule);
+        GuessMyNumber backward = new GuessMyNumber(player2, player1, rule);
+        int result = 0;
+
         for (int k = 0; k < rounds; k++) {
-            /* TODO: Ein Hinspiel. Ergebnis zu result addieren. */;
-            /* TODO: Ein Rückspiel. Ergebnis von result subtrahieren. */;
+            result += forward.playRound();
+            result -= backward.playRound();
         }
+
         return result;
     }
 }
